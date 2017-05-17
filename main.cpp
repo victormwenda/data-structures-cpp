ffdbad426258cca6d71fde59731412e8723b6485
Victor Mwenda <vmwenda.vm@gmail.com>
2017-05-17 09:48:10 +0300

    Reset project files

--- main.cpp
+++ main.cpp
@@ -1,29 +1,8 @@
 #include <iostream>
-#include "Stack.h"
 
 using namespace std;
 
 int main() {
-    Stack<int> integers{10};
 
-    if(integers.isEmpty()){
-        printf("is  empty \n");
-    }
-
-    for (int i = 0; i < 11; i++) {
-        integers.add(i);
-        printf("The item at %i is %i\n", i, integers.peek());
-    }
-
-    int *items = integers.list();
-
-    for(int i =0; i < integers.getItemCount();i++){
-        printf("Found item %i\n", items[i]);
-    }
-    integers.popAll();
-
-    if(integers.isEmpty()){
-        printf("is  empty\n");
-    }
     return 0;
 }
